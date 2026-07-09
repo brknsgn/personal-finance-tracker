94bf1b1382996254d52cb69149563be845aca70f
brknsgn <brknsgn123@gmail.com>
2026-07-09 17:43:48 +0300

    feat: Implement Filters component with category and date selection

--- frontend/src/components/Filters.jsx
+++ frontend/src/components/Filters.jsx
@@ -1,7 +1,46 @@
-export default function Filter() {
+export default function Filters({
+  selectedCategory, setSelectedCategory,
+  selectedDate, setSelectedDate
+}) {
   return (
-    <nav>
-      <h2>Filter Component</h2>
-    </nav>
-  )
+    <div style={{
+      padding: '15px',
+      border: '1px sold #ccc',
+      marginBottom: '20px',
+      display:'flex',
+      gap: '20px',
+      alignItems: 'center',
+      backgroundColor: '#f9f9f9'
+    }}>
+      <h4 style= {{ margin: 0}}>Filters:</h4>
+      <div>
+        <label style= {{marginRight: '10px'}}>Category:</label>
+        <select
+        value={selectedCategory}
+        onChange={(e) => setSelectedCategory(e.target.value)}>
+          <option value="all">All Categories</option>
+          <option value="food">Food</option>
+          <option value="rent">Rent</option>
+          <option value="salary">Salary</option>
+          <option value="entertainment">Entertainment</option>
+          <option value="other">Other</option>
+        </select>
+        </div>
+        <div>
+          <label stle={{ marginRight: '10px'}}>Date:</label>
+          <input
+          type="date"
+          value={selectedDate}
+          onChange={(e) => setSelectedDate(e.target.value)}/>
+          {selectedDate &&(
+            <button
+            onClick={() =>setSelectedDate('')}
+            style={{marginLeft: '10px', cursor: 'pointer'}}>
+              Clear Date
+            </button>
+          )}
+        </div>
+</div>
+  );
+    
 }
